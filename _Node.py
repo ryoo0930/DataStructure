6a8f39817b8d5680b8584b8c64e120bb2c5901b7
ryoo0930 <ryoo0930@naver.com>
2025-03-17 11:25:50 +0900

    Node

---
+++ _Node.py
@@ -0,0 +1,8 @@
+class _Node:
+    """LightWeight, nonpublic class for storing a singly linked node"""
+    __slot__ = '_element' '_next' '_prev'
+    
+    def __init__(self, element, next, prev):
+        self._element = element
+        self._next = next
+        self._prev = prev
