69f7695f409110fe23c4d58295910bad03ffe7e4
ryoo0930 <ryoo0930@naver.com>
2025-03-18 13:33:48 +0900

    DoublyLinkedBase

--- DoublyLinkedBase.py
+++ DoublyLinkedBase.py
@@ -1,33 +1,41 @@
-from _Node import _Node
-
 class _DoublyLinkedBase:
     
+    class _Node:
+        """LightWeight, nonpublic class for storing a singly linked node"""
+        __slot__ = ('_element', '_next', '_prev')
+    
+        def __init__(self, element, prev, next):
+            self._element = element
+            self._prev = prev
+            self._next = next
+    
     def __init__(self):
-        self.header = _Node(None)
-        self.tailer = _Node(None)
-        self.header._next = self.tailer
-        self.tailer._prev = self.header
+        self._header = self._Node(None, None, None)
+        self._trailer = self._Node(None, None, None)
+        self._header._next = self._trailer
+        self._trailer._prev = self._header
         self._size = 0
-        
+    
     def __len__(self):
         return self._size
     
     def is_empty(self):
         return self._size == 0
     
-    def _insert_between(self, element, PrevNode, NextNode):
-        newNode = _Node(element, NextNode, PrevNode)
-        PrevNode._next = newNode
-        NextNode._prev = newNode
+    def _insert_between(self, e, predecessor, successor):
+        newNode = self._Node(e, predecessor, successor)
+        predecessor._next = newNode
+        successor._prev = newNode
         self._size += 1
         return newNode
     
     def _delete_node(self, node):
-        PrevNode = node._prev
-        NextNode = node._next
-        PrevNode._next = NextNode
-        NextNode._prev = PrevNode
+        predecessor = node._prev
+        successor = node._next
+        predecessor._next = successor
+        successor._prev = predecessor
         self._size -= 1
         element = node._element
         node._prev = node._next = node._element = None
         return element
+    
