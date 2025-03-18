22d310cf5cbecc037a0a8a46d2c326c865167558
ryoo0930 <ryoo0930@naver.com>
2025-03-18 21:05:02 +0900

    test

--- Test.py
+++ Test.py
@@ -5,5 +5,14 @@ pl.add_first(10)
 pl.add_first(20)
 pl.add_last(40)
 
+print(list(pl))
+
+pos = pl.first()
+pl.add_after(pos, 30)
+
+print(list(pl))
+
+pos = pl.after(pl.first())
+pl.add_before(pos, 90)
 
 print(list(pl))
