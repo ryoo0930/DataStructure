25b957350b0f8cce3e0361b57e2156bd009029c7
ryoo0930 <ryoo0930@naver.com>
2025-03-19 11:03:09 +0900

    Quick Sort

--- AlgorithmTest.py
+++ AlgorithmTest.py
@@ -1,5 +1,5 @@
 from LinkedQueue import _LinkedQueue
-from MergeSort_SinglyLinkedList import merge_sort
+from QuickSort import quick_sort
 
 lq = _LinkedQueue()
 
@@ -9,8 +9,19 @@ lq.enqueue(80)
 lq.enqueue(70)
 lq.enqueue(40)
 lq.enqueue(10)
+
+print("정렬 전 리스트 :")
+for i in range(len(lq)):
+    print(lq.dequeue())
     
-merge_sort(lq)
+lq.enqueue(50)
+lq.enqueue(30)
+lq.enqueue(80)
+lq.enqueue(70)
+lq.enqueue(40)
+lq.enqueue(10)
+    
+quick_sort(lq)
 
 print("정렬 후 리스트 :")
 for i in range(len(lq)):
