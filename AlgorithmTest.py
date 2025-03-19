731b88c7d5373319b78338a3a0a4e8e984eb5214
ryoo0930 <ryoo0930@naver.com>
2025-03-19 10:36:47 +0900

    Merge_Sort

--- AlgorithmTest.py
+++ AlgorithmTest.py
@@ -1,10 +1,17 @@
 from LinkedQueue import _LinkedQueue
+from MergeSort_SinglyLinkedList import merge_sort
 
 lq = _LinkedQueue()
 
-for i in range(5):
-    value = 100 - (i * 10)
-    lq.enqueue(value)
+lq.enqueue(50)
+lq.enqueue(30)
+lq.enqueue(80)
+lq.enqueue(70)
+lq.enqueue(40)
+lq.enqueue(10)
     
-for i in range(5):
+merge_sort(lq)
+
+print("정렬 후 리스트 :")
+for i in range(len(lq)):
     print(lq.dequeue())
